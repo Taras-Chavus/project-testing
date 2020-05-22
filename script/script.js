71a5235ccb6fa959a5e9403332145ecc8cdf3a46
Taras-Chavus <57559681+Taras-Chavus@users.noreply.github.com>
2020-05-22 19:31:23 +0300

    sdqdewde

--- script/script.js
+++ script/script.js
@@ -3,3 +3,5 @@ $('.menu-btn').on('click', function(e) {
     $(this).toggleClass('menu-btn_active');
     $('.menu-nav').toggleClass('menu-nav_active') && $('.section').toggleClass('section-active');
   });
+
+  
